57294e4c9a051d2cee44358bf1d8c7901cde6aa5
jason <jason@jasons-mbp.lan>
2019-01-14 18:24:05 +0000

    Refactored and added comments

--- CognitoLogon/lambda.js
+++ CognitoLogon/lambda.js
@@ -1,10 +1,16 @@
+//Log on to Cognito, establish AWS credentials then display the result 
 function testLambda(params) {
 	
-//	var user = 'testUser';
-//	var pass = 'Password1';
+//	Single user available 'testUser' with password 'Password1';
 	
 	var user = params.formUserName.value;
 	var pass = params.formPassword.value
+	
+	var region = 'eu-west-2';
+	var userPool = 'eu-west-2_6Qk8UHkl5';
+	var cognitoAppId = '41pboo7igtsbm6bfi18sje5p96';
+	var identityPool = 'eu-west-2:72c461e0-ca5f-47ce-882e-bee6cc92812b';
+	var cognitoLogin = 'cognito-idp.'+region+'.amazonaws.com/'+userPool;
 	
 	var authenticationData = {
         Username : user,
@@ -17,8 +23,8 @@ function testLambda(params) {
 	var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
 
 	var poolData = {
-			UserPoolId : 'eu-west-2_6Qk8UHkl5',
-			ClientId : '41pboo7igtsbm6bfi18sje5p96'
+			UserPoolId : userPool,
+			ClientId : cognitoAppId
 	};
 	var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
@@ -27,33 +33,36 @@ function testLambda(params) {
 			Pool : userPool
 	};
 	
-	AWS.config.region = 'us-west-2';
+	AWS.config.region = region;
 
+	//Authenticate with Cognito
     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
     cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: function (result) {
             var accessToken = result.getAccessToken().getJwtToken();
             var idToken = result.idToken.jwtToken;
             
-            console.log(idToken);
+            //console.log(idToken);
             
+            //Cognito logon sucessful. Update our configuration with our JWT Token
             AWS.config.update({
 	              credentials: new AWS.CognitoIdentityCredentials({
-	              	IdentityPoolId: 'eu-west-2:72c461e0-ca5f-47ce-882e-bee6cc92812b',
+	              	IdentityPoolId: identityPool,
 	                Logins: {
-	                    'cognito-idp.eu-west-2.amazonaws.com/eu-west-2_6Qk8UHkl5': result.getIdToken().getJwtToken()
+	                	[cognitoLogin] : result.getIdToken().getJwtToken()
 	                }
 	              }),
-	              region: 'eu-west-2'
+	              region: region
 	            });
             
+            //Now use our cognito logon to get our AWS credentials
             AWS.config.credentials.get(function(err) {
 				if (err) {
 					console.log(err);
 				} else {
 					console.log("Retrieved credentials");
 					
-		            //Call lambda
+		            //AWS credentials are now established - call the lambda
 		            var lambda = new AWS.Lambda();
 					var payload = {
 						token : idToken
@@ -64,24 +73,27 @@ function testLambda(params) {
 					  LogType: 'Tail',
 					  Payload: JSON.stringify(payload),
 					};
+					
+					//Invoke the lambda and parse the response
 					lambda.invoke(params, function(err, data) {
 					  if (err) {
 					  	console.log(err, err.stack);
 					  } else {
 						console.log('Lambda called');
-					    alert('Sucessful Cognito Log On')
+						var response = JSON.parse(data.Payload);
+					    alert('Sucessful Cognito Log On. Lambda executed: '+response.body);
 					  }
 					});
 				}
 			});
-            
-
         },
 
+        //Login failure
         onFailure: function(err) {
             alert(err);
         },
         
+        //New password flow. Only used for first log in
 	    newPasswordRequired: function(usrAttributes, requiredAttributes) {	    	
 	    	alert('New password required');
 	    	delete usrAttributes.email_verified;
