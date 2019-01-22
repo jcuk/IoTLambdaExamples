eab01a5b353f04e27485f53a9d3aaf0336bf7ed0
jason <jason@jasons-mbp.lan>
2019-01-22 11:37:14 +0000

    Update credentials with identity pool

--- AdminPanel/cognito.js
+++ AdminPanel/cognito.js
@@ -1,16 +1,21 @@
-//Log on to Cognito then display the result
 var cognitoUser;
 
+const region = 'eu-west-2';
+
+AWS.config.region = region;
+
+const userPoolId = 'eu-west-2_6Qk8UHkl5';
+const cognitoAppId = '41pboo7igtsbm6bfi18sje5p96';
+const identityPool = 'eu-west-2:72c461e0-ca5f-47ce-882e-bee6cc92812b';
+const cognitoLogin = 'cognito-idp.'+region+'.amazonaws.com/'+userPoolId;
+
+//Log on to Cognito then display the result
 function cognitoLogon(params) {
 
 //	Single user available 'testUser' with password 'Password1';
 
 	var user = params.formUserName.value;
 	var pass = params.formPassword.value
-
-	var region = 'eu-west-2';
-	var userPool = 'eu-west-2_6Qk8UHkl5';
-	var cognitoAppId = '41pboo7igtsbm6bfi18sje5p96';
 
 	var authenticationData = {
 			Username : user,
@@ -23,7 +28,7 @@ function cognitoLogon(params) {
 	var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
 
 	var poolData = {
-			UserPoolId : userPool,
+			UserPoolId : userPoolId,
 			ClientId : cognitoAppId
 	};
 	var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
@@ -40,6 +45,26 @@ function cognitoLogon(params) {
 	cognitoUser.authenticateUser(authenticationDetails, {
 		onSuccess: function (result) {
 			alert('Sucessful Cognito Log On');
+
+			var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+			cognitoUser.authenticateUser(authenticationDetails, {
+				onSuccess: function (result) {
+					var idToken = result.idToken.jwtToken;
+
+//					console.log(idToken);
+
+					//Cognito logon sucessful. Update our configuration with our JWT Token
+					AWS.config.update({
+						credentials: new AWS.CognitoIdentityCredentials({
+							IdentityPoolId: identityPool,
+							Logins: {
+								[cognitoLogin] : idToken
+							}
+						}),
+						region: region
+					});
+				}
+			});
 		},
 
 		//Login failure
@@ -62,19 +87,29 @@ function changeAttributeValue(params) {
 		return;
 	}
 	
-    var attributeList = [];
-    var attribute = {
-        Name : 'custom:'+params.formAttribute.value,
-        Value : params.formAttributeValue.value
-    };
-    var attribute = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
-    attributeList.push(attribute);
+	const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
+		apiVersion: '2016-04-18'
+	});
+	
+	var attributeList = [];
+	var attribute = {
+			Name : 'custom:'+params.formAttribute.value,
+			Value : params.formAttributeValue.value
+	};
 
-    cognitoUser.updateAttributes(attributeList, function(err, result) {
-        if (err) {
-            alert(err);
-            return;
-        }
-        alert('Set Attribute: ' + result);
-    });
+	attributeList.push(attribute);
+	
+	cognitoidentityserviceprovider.adminUpdateUserAttributes( {
+			UserAttributes: attributeList,
+			UserPoolId: userPoolId,
+			Username: 'testUser'
+		},
+		function(err, data) {
+			if (err){
+				alert("Error updating attributes: "+err);
+			} else {
+				alert("Updated attribute");
+			}
+		}
+	);
 }
